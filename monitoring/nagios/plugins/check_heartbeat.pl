7bb57f3c8cba27f8cfb53cebe05a94d8a367e8a3
kissit <brian@kissitconsulting.com>
2015-10-05 18:54:39 -0400

    Update for licenses

--- monitoring/nagios/plugins/check_heartbeat.pl
+++ monitoring/nagios/plugins/check_heartbeat.pl
@@ -1,10 +1,49 @@
 #!/usr/bin/env perl
+#
+## check_heartbeat.pl
+##
+## Copyright (C) 2015 KISS IT Consulting <http://www.kissitconsulting.com/>
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions
+## are met:
+## 
+## 1. Redistributions of source code must retain the above copyright
+##    notice, this list of conditions and the following disclaimer.
+## 2. Redistributions in binary form must reproduce the above
+##    copyright notice, this list of conditions and the following
+##    disclaimer in the documentation and/or other materials
+##    provided with the distribution.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL ANY
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+## EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+## PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+## OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+## NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+## SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+##
+## Just a simple Perl script that you can call to check the status of a heartbeat from the Percona Toolkit.
+## Its meant to be called from NRPE but not required I guess.
+##
+## Installation:
+## 1. Pull down the script into your nagios plugin directory.
+## 2. Set it to be executable as desired, for instance:
+##      chmod 755 /usr/lib64/nagios/plugins/check_heartbeat.pl
+## 3. Add an entry into NRPE config on the MySQL slave(s) such as:
+##      command[check_mysql_replication]=/opt/nagios/libexec/check_mkheartbeat.pl -H localhost -u username -p password -w 300 -c 400
+## 4. Configure a service check in nagios server to call the NRPE check.
+##
+
 use strict;
 use Getopt::Long;
 use DBI;
 require Carp;
 
-my $options = { 'H' => 'localhost', 'r' => 3306, 'w' => 180, 'c' => 300 , 'u' => 'monitor', 'p' => 'password', 'd' => 'percona' };
+my $options = { 'H' => 'localhost', 'r' => 3306, 'w' => 180, 'c' => 300 , 'u' => 'username', 'p' => 'password', 'd' => 'percona' };
 
 GetOptions($options, "H=s", "r=i", "w=i", "c=i", "u=s", "p=s", "d=s", "help");
 
